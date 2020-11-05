ed20c68d7dc3797a6bca15a076d663af67c7b0e9
kkfuturetech <73984449+kkfuturetech@users.noreply.github.com>
2020-11-04 23:02:45 -0800

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,39 @@
+myFunction([
+	{
+	"shape": [
+	  
+	  
+		{
+		  "shapeName":"Rectangle",
+		  "input":[ "length", "breadth"],
+		  "output":[ "Area", "Perimeter" ],
+		  "formula":["length*breadth","2*(length+breadth)"]
+		},
+	 
+		{
+			"shapeName":"Square",	
+		  "input":[ "length"],
+		  "output":[ "Area", "Perimeter" ],
+		  "formula":["length*length","4*length"]
+		}  ,
+	 
+		{
+			"shapeName":"3d",	
+		  "input":[ "length", "breadth","height"],
+		  "output":[ "Area", "Perimeter" ],
+		  "formula":["length*breadth*height","2*(length+breadth+height)"]
+		}  ,
+	 
+		{
+			"shapeName":"triangle",	
+		  "input":[ "base","height"],
+		  "output":[ "Area" ],
+		  "formula":["0.5*base*height"]
+		}  
+	
+	]
+  }
+])
+
+
+
